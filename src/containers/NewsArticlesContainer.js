79a5d703270cef20a5a4b79a1e767c156c90e0ea
Andrew Thomas <ajthomas145@gmail.com>
2021-10-22 16:14:48 +0100

    data from api on container

--- src/containers/NewsArticlesContainer.js
+++ src/containers/NewsArticlesContainer.js
@@ -1,7 +1,20 @@
 import React, {useState, useEffect} from 'react';
 
 const NewsArticlesContainer = () => {
-    const[articles, setArticles] = useState([]);
+    const [articles, setArticles] = useState([]);
+
+    
+    const getArticles = function(){
+        fetch('https://content.guardianapis.com/search?q=brexit&format=json&api-key=test')
+        .then(res => res.json())
+        .then(articles => setArticles(articles))
+        // .then(articles => setArticles(articles))
+        // console.log(articles);
+    }
+
+    useEffect(() => {
+        getArticles()
+    }, [])
 
 
     return(
