1d9cfc1055aca91286f485584ba3bdba936a4e62
Andrew Thomas <ajthomas145@gmail.com>
2021-10-22 15:47:45 +0100

    initial setup

---
+++ src/containers/NewsArticlesContainer.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
+const NewsArticlesContainer = () => {
+    const[articles, setArticles] = useState([]);
+
+
+    return(
+        <div>
+            Hello
+        </div>
+    )
+}
+
+
+export default NewsArticlesContainer;
